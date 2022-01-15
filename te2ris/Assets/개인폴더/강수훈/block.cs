e5fa6815add97d56b2dc074f833d6648851a1af3
강수훈 <92258022+ginikang@users.noreply.github.com>
2022-01-15 20:12:01 +0900

    블록이동 수정본

--- te2ris/Assets/개인폴더/강수훈/block.cs
+++ te2ris/Assets/개인폴더/강수훈/block.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class block : MonoBehaviour
 {
-    
+    [SerializeField]
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,31 +22,30 @@ public class block : MonoBehaviour
     }
     private void rotate()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if(Input.GetKeyDown(KeyCode.LeftShift))                 //왼쪽 쉬프트 누르면
         {
-            transform.Rotate(0, 0, +90);
+            transform.Rotate(0, 0, +90);                    //90도 회전 시키기
         }
     }
-    private void move()
+
+    private void move()                     
     {
-        Vector2 pos = transform.position;
-        if(Input.GetKeyDown(KeyCode.A))
+        Vector2 pos = transform.position;               //위치 옮겨적을 새로운 변수 생성
+        if(Input.GetKey(KeyCode.A))                         //a누르면 
         {
-            pos.x -= 0.5f;
+            pos.x -= Time.deltaTime * speed;                   // 변수에서 x 값을 뺌
             
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            pos.x += 0.5f;
-            
+            pos.x += Time.deltaTime * speed;
         }
-        
-        transform.position = pos;
+        transform.position = pos;                       //새로운 변수의 변경된 값을 현재 위치에 대입
     }
     private void limit_move()
     {
-        Vector3 limit_pos = Camera.main.WorldToViewportPoint(transform.position);
-        if (limit_pos.x < 0.05f)
+        Vector3 limit_pos = Camera.main.WorldToViewportPoint(transform.position);           //화면 비율변수 생성
+        if (limit_pos.x < 0.05f)                //밖으로 나가면 안쪽으로 텔레포트 시킴
         {
             limit_pos.x = 0.05f;
         }
@@ -52,6 +53,6 @@ public class block : MonoBehaviour
         {
             limit_pos.x = 0.95f;
         }
-        transform.position = Camera.main.ViewportToWorldPoint(limit_pos);
+        transform.position = Camera.main.ViewportToWorldPoint(limit_pos);           //화면 비율변수를 게임 화면에 대입
     }
 }
--- te2ris/Assets/개인폴더/강수훈/player.cs
+++ te2ris/Assets/개인폴더/강수훈/player.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public float speed;
-    public float power;
-    private int canjump = 1;
-    Rigidbody2D myrigid;
+    public float speed;                     //플레이어 움직임 나타내는 변수
+    public float power;                     //점프파워 나타내는 변수
+    private int canjump = 1;                //점프 가능여부를 나타내는 변수
+    Rigidbody2D myrigid;                    //리지드바디 갖고오는 변수
 
     // Start is called before the first frame update
     void Start()
     {
-        myrigid = GetComponent<Rigidbody2D>();
+        myrigid = GetComponent<Rigidbody2D>();              //리지드바디 갖고옴
 
     }
 
@@ -23,10 +23,11 @@ public class player : MonoBehaviour
         jump();
         limit_move();
     }
-    private void move()
+
+    private void move()                     //플레이어 움직임 함수
     {
-        Vector3 pos = transform.position;
-        if (Input.GetKey(KeyCode.LeftArrow))
+        Vector3 pos = transform.position;       //위치값을 담을 새로운 변수 생성
+        if (Input.GetKey(KeyCode.LeftArrow))            //키 누르면 변수값을 가감시킴
         {
             pos.x -= Time.deltaTime * speed;
 
@@ -36,12 +37,13 @@ public class player : MonoBehaviour
             pos.x += Time.deltaTime * speed;
 
         }
-        transform.position = pos;
+        transform.position = pos;               //새로운 변수를 현재 위치에 대입시킴
     }
-    private void limit_move()
-    {
-        Vector3 limit_pos = Camera.main.WorldToViewportPoint(transform.position);
-        if( limit_pos.x <0.05f)
+
+    private void limit_move()                       //화면밖으로 못나가게 하는 함수
+    {       //위치를 화면비율(0,0)~(1,1)로 나타냄
+        Vector3 limit_pos = Camera.main.WorldToViewportPoint(transform.position);       
+        if( limit_pos.x <0.05f)                     //밖으로 나가면 텔레포트 시킴
         {
             limit_pos.x = 0.05f;
         }
@@ -49,21 +51,26 @@ public class player : MonoBehaviour
         {
             limit_pos.x = 0.95f;
         }
-        transform.position = Camera.main.ViewportToWorldPoint(limit_pos);
+        //화면 비율을 현재 위치로 다시 바꿈
+        transform.position = Camera.main.ViewportToWorldPoint(limit_pos);           
     }
-    private void jump()
+
+    private void jump()         //점프함수
     {
-        if(Input.GetKeyDown(KeyCode.RightShift) && canjump==1)
+        if(Input.GetKeyDown(KeyCode.RightShift) && canjump==1)              //키를 누르고 점프 가능한 상태일 경우
         {
-            myrigid.velocity = Vector2.up*power;
-            canjump = 0;
+            myrigid.velocity = Vector2.up*power;                    //상단으로 힘을 받음
+            canjump = 0;                                            //점프할경우 점프 불가 상태가 됨
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)          //물체 충돌 확인 함수
     {
-        if(collision.gameObject.tag == "ground")
+        if(collision.gameObject.tag == "ground")            //태그가 그라운드,블록인 물체와 충돌할경우
         {
-            canjump = 1;
+            canjump = 1;            //점프 가능
         }
         if (collision.gameObject.tag == "block")
         {
