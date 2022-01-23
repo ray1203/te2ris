977dd6e546adcae32ff12057dd63763a2dad15d1
RightHennessy <myheaven_b@naver.com>
2022-01-23 19:45:19 +0900

    add : pausePage

--- te2ris/Assets/개인폴더/최예린/script/PausePage.cs
+++ te2ris/Assets/개인폴더/최예린/script/PausePage.cs
@@ -1,35 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PausePage : MonoBehaviour
 {
-    public static bool GameIsPaused = false; 
-    public GameObject pauseMenuCanvas; 
-    
+    public string thisScene;
+    public GameObject pauseMenu;
+
+    void Start(){
+        thisScene = SceneManager.GetActiveScene().name;  
+    }
+
     void Update() { 
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (GameIsPaused) 
-                Resume();  
-            else 
-                Pause(); 
+        if (Input.GetKey(KeyCode.Escape)) {
+            Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
         } 
     }
 
+    public void Restart(){
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(thisScene);
+    }
+
     public void Resume() {
-        pauseMenuCanvas.SetActive(false); 
-        Time.timeScale = 1f; 
-        GameIsPaused = false; 
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
 
-    public void Pause() { 
-        pauseMenuCanvas.SetActive(true); 
-        Time.timeScale = 1f; 
-        GameIsPaused = true; 
+    public void startPage(){
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync("start");
     }
 
-    
-    
 
-    
+
+
+
+
 }
