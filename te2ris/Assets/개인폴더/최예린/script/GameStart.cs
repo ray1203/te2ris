ad4779c9c6698a0df875336c2688167e0653335e
lee jeongan <fkdl000048@gmail.com>
2022-02-17 16:41:58 +0900

    시작 사운드 및 효과음

--- te2ris/Assets/개인폴더/최예린/script/GameStart.cs
+++ te2ris/Assets/개인폴더/최예린/script/GameStart.cs
@@ -5,8 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    public AudioClip clip;
+
     public void StartGame()
     {
+        StartCoroutine(select());
+    }
+
+    IEnumerator select()
+    {
+        SoundManager.instance.SFXPlay("coin", clip);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("StageSelect");
     }
 }
