9d8469685cf8484d2ea57a18f89545e330d76c16
ray12031203 <ray12031203@gmail.com>
2022-02-18 13:14:19 +0900

    스테이지 클리어 상황에 따른 별 생성

---
+++ te2ris/Assets/Scripts/Json/StageStarSet.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StageStarSet : MonoBehaviour
+{
+    public Sprite star1, star2, star3, star4;
+    public GameObject star, big_star;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] starList = GameObject.Find("StageData").GetComponent<StageSave>().GetAllStars();
+        for(int i = 1; i <= 9; i++)
+        {
+            if (starList[i] == 1)
+            {
+                GameObject newObject = Instantiate(star);
+                newObject.GetComponent<Image>().sprite = star1;
+                newObject.transform.parent = transform.Find("Stage" + i);
+                newObject.transform.localScale = new Vector3(1, 1, 1);
+                newObject.transform.localPosition = new Vector2(0, 24);
+            }
+            else if (starList[i] == 2)
+            {
+                GameObject newObject = Instantiate(star);
+                newObject.GetComponent<Image>().sprite = star2;
+                newObject.transform.parent = transform.Find("Stage" + i);
+                newObject.transform.localScale = new Vector3(1, 1, 1);
+                newObject.transform.localPosition = new Vector2(0, 24);
+            }
+            else if (starList[i] == 3)
+            {
+                GameObject newObject = Instantiate(star);
+                newObject.GetComponent<Image>().sprite = star3;
+                newObject.transform.parent = transform.Find("Stage" + i);
+                newObject.transform.localScale = new Vector3(1, 1, 1);
+                newObject.transform.localPosition = new Vector2(0, 24);
+            }
+            else if (starList[i] == 4)
+            {
+                GameObject newObject = Instantiate(big_star);
+                newObject.GetComponent<Image>().sprite = star4;
+                newObject.transform.parent = transform.Find("Stage" + i);
+                newObject.transform.localScale = new Vector3(1, 1, 1);
+                newObject.transform.localPosition = new Vector2(0, 25);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
